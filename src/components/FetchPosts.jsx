9c2f4cc312aba63a72d44a5746707dbd15b47cd4
Abhishek Joshi <joshiabhishek2255@gmail.com>
2023-11-27 13:31:10 +0530

    Final Push

--- src/components/FetchPosts.jsx
+++ src/components/FetchPosts.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Postss = ({ data }) => {
     return (
-        <div className=' p-1'>{data && data.map((post) => (
+        <div className=' p-3'>{data && data.map((post) => (
             <div className="card m-2" style={{ width: "100%" }} key={post.id}>
                 <div className="card-body">
                     <h5 className="card-title">{post.title}</h5>
